5cdcd5ca2b45e5d4224fb1d06d04568d706f3d21
Christoffer Niska <christofferniska@gmail.com>
2022-05-23 23:41:13 +0300

    refactor: Cleanup

--- README.md
+++ README.md
@@ -9,3 +9,45 @@ Run the game
 ```sh
 yarn start
 ```
+
+## Output
+
+Example output for a single game:
+
+```sh
+K1 hits K2 for 6 damage
+K2 hits K3 for 4 damage
+K3 hits K4 for 4 damage
+K4 hits K5 for 5 damage
+K5 hits K1 for 2 damage
+K1 hits K2 for 1 damage
+K2 hits K3 for 4 damage
+K3 hits K4 for 6 damage
+K4 is defeated
+K5 hits K1 for 4 damage
+K1 hits K2 for 1 damage
+K2 hits K3 for 2 damage
+K3 is defeated
+K5 hits K1 for 1 damage
+K1 hits K2 for 3 damage
+K2 is defeated
+K5 hits K1 for 1 damage
+K1 hits K5 for 4 damage
+K5 hits K1 for 4 damage
+K1 is defeated
+K5 hits K5 for 5 damage
+K5 is defeated
+K6 is the winner
+```
+
+## Improvement ideas
+
+* [x] Make constants easy to configure
+* [x] Create a factory for knights
+* [x] Do not mutate the knights array
+* [x] Use a range for the damage
+* [ ] Add some unit tests
+* [ ] Introduce a game state
+* [ ] Use reducers to update the game state
+* [ ] Use a linked list for the knights
+* [ ] Move knight logic into a `Knight` class
--- src/index.ts
+++ src/index.ts
@@ -1,53 +1,45 @@
+const MAX_HITPOINTS = 10;
+const DAMAGE_RANGE = [1, 6];
+
 type Knight = {
   name: string;
   hitpoints: number;
 };
 
-const run = () => {
-  const knights: Knight[] = [
-    {name: 'K1', hitpoints: 10},
-    {name: 'K2', hitpoints: 10},
-    {name: 'K3', hitpoints: 10},
-    {name: 'K4', hitpoints: 10},
-    {name: 'K5', hitpoints: 10},
-    {name: 'K6', hitpoints: 10},
-  ];
-
+(function run() {
+  let knights = createKnights(['K1', 'K2', 'K3', 'K4', 'K5', 'K6']);
   let currentIndex = 0;
 
   while (knights.length > 1) {
     const knight = knights[currentIndex];
-    const targetIndex = getNextIndex(currentIndex, knights);
+    const targetIndex = nextIndex(currentIndex, knights);
     const target = knights[targetIndex];
-    const damage = getRandomArbitrary(1, 6);
+    const damage = randomInRange(DAMAGE_RANGE);
 
     target.hitpoints -= damage;
-    console.log(
-      '%s attacks %s for %d damage',
-      knight.name,
-      target.name,
-      damage
-    );
+    console.log('%s hits %s for %d damage', knight.name, target.name, damage);
 
     if (target.hitpoints <= 0) {
-      knights.splice(targetIndex, 1);
+      knights = knights.filter((_k, i) => i !== targetIndex);
       console.log('%s is defeated', target.name);
     }
 
-    currentIndex = getNextIndex(currentIndex, knights);
+    currentIndex = nextIndex(currentIndex, knights);
   }
 
   console.log('%s is the winner', knights[0].name);
-};
+})();
 
-function getNextIndex(currentIndex: number, items: unknown[]) {
-  return currentIndex < items.length - 2 ? currentIndex + 1 : 0;
+function createKnights(names: string[]): Knight[] {
+  return names.map(name => ({name, hitpoints: MAX_HITPOINTS}));
 }
 
-function getRandomArbitrary(min: number, max: number) {
+function nextIndex<T>(currentIndex: number, list: T[]) {
+  return currentIndex < list.length - 2 ? currentIndex + 1 : 0;
+}
+
+function randomInRange([min, max]: number[]) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
-
-run();
